84169df2f373471170ad97165fdd71a59d3cb27f
Aman jha <ajha82703@gmail.com>
2026-05-30 10:38:22 +0530

    refactor: fix technical debt node (missing-try-catch)

--- app/routes/error.js
+++ app/routes/error.js
@@ -1,15 +1,21 @@
 // Error handling middleware
 
 const errorHandler = (err, req, res,next) => {
-
-    "use strict";
-
-    console.error(err.message);
-    console.error(err.stack);
-    res.status(500);
-    res.render("error-template", {
-        error: err
-    });
+    try {    
+    
+        "use strict";
+    
+        console.error(err.message);
+        console.error(err.stack);
+        res.status(500);
+        res.render("error-template", {
+            error: err
+        });
+    
+    } catch (error) {
+      console.error("Error occurred in operation:", error);
+      throw error;
+    }
 };
 
 module.exports = { errorHandler };
